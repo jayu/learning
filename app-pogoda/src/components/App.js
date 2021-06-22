f188ef7976b795558bf78bced680d2da43505308
Karton Pierwszy <kartonpierwszy@gmail.com>
2021-06-22 13:34:27 +0200

    Cz4. Fetch method. ps. Is not working

--- app-pogoda/src/components/App.js
+++ app-pogoda/src/components/App.js
@@ -6,6 +6,14 @@ import './App.css';
 class App extends Component {
   state = {
     value: '',
+    date: '',
+    city: '',
+    sunrise: '',
+    sunset: '',
+    temp: '',
+    pressure: '',
+    wind: '',
+    err: '',
   };
 
   handleInputChange = (e) => {
@@ -14,10 +22,31 @@ class App extends Component {
     });
   };
 
+  handleCitySubmit = (e) => {
+    e.preventDefault();
+    // console.log('potwierdzony formularz');
+    const API = `api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=29e41f2bae6fbf235cc88ac35096ca68&units=metric`;
+
+    fetch(API)
+      .then((response) => {
+        if (response.ok) {
+          return response;
+        }
+        throw Error('Nie udało się');
+      })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  };
+
   render() {
     return (
       <div className='App'>
-        <Form value={this.state.value} change={this.handleInputChange} />
+        <Form
+          value={this.state.value}
+          change={this.handleInputChange}
+          submit={this.handleCitySubmit}
+        />
         <Result />
       </div>
     );
